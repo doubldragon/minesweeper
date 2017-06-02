3972232a0fdc4ddb083c57634a7479947b50a221
Robert Bryan <bryarobert@gmail.com>
2017-06-02 16:46:09 -0400

    added setAttribute data

--- src/js/default.js
+++ src/js/default.js
@@ -3,43 +3,45 @@ let HTMLboard = document.getElementById("board");
 
 // board variables
 let size = 10,
-    board = new Array(size * size).fill(false);
+    board = [];
 
-// cell constructor funciton
-let Cell = (() => {
-  let nextId = 0;
-   return function Cell() {
-      this.id = nextId++;
-      this.revealed = false;
-      this.clickable = true;
-      this.isMine = false;
-      this.touchingMines;
-      this.flag = false; 
-      this.mineCheck = () => {
-        if (this.isMine){
-            console.log("boom");
-        } 
-      };
-   }
-})();
-
-var $00 = new Cell();
-
-console.log($00);
+// // cell constructor function
+// let Cell = (() => {
+//   let nextId = 0;
+//    return function Cell() {
+//       this.id = nextId++;
+//       this.revealed = false;
+//       this.clickable = true;
+//       this.isMine = false;
+//       this.touchingMines;
+//       this.flag = false; 
+//       this.mineCheck = () => {
+//         if (this.isMine){
+//             console.log("boom");
+//         } 
+//       };
+//    }
+// })();
 
 function boardGeneration (x) {
     
     for (var i = 0; i < x; i++) {
+        board.push([]); 
         for (var j = 0; j < x; j++) {
             let div = document.createElement("div");
-            HTMLboard.appendChild(div).id = i + "_" + j ;
+            div.setAttribute("data-isMine", false);
+            div.setAttribute("data-revealed", false);
+            div.setAttribute("data-flag", false);
+            div.setAttribute("data-question", false);
+            div.setAttribute("data-touching", 0);
+            HTMLboard.appendChild(div).id = i + "_" + j;
+            board[i].push([]);
         }
     }  
 }
-
+console.log(board);
 boardGeneration(size);
 
-// make array of 10 x 10 empty then generate the divs with unique ids.
-// then pick n amount of random ids and switch them to this.is mine
+
 
 
